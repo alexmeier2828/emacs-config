9ecd0607db4d98890c723aea60dbddb5ea0c0b1f
alexmeier2828 <ameier42@gmail.com>
2024-04-20 14:21:15 -0400

    Cleaned up config, starting from scratch

--- init.el
+++ init.el
@@ -1,31 +1,18 @@
+(require 'org)
+(org-babel-load-file
+ (expand-file-name "settings.org"
+		   user-emacs-directory))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(tango-dark))
- '(org-agenda-files '("~/Google Drive/My Drive/DriveSyncFiles/org"))
- '(org-capture-templates
-   '(("t" "Adds a todo" entry
-      (file "notes.org")
-      "* TODO %? ")))
- '(org-directory "~/Google Drive/My Drive/DriveSyncFiles/org")
- '(package-selected-packages '(magit evil)))
+ '(package-selected-packages
+   '(magit doom-modeline doom-mode-line powerline which-key marginalia orderless corfu gruvbox-theme evil)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-
-;; org-agenda
-
-;; text mode hooks
-(evil-mode)
-
-(global-set-key (kbd "C-c a") #'org-agenda)
-(global-set-key (kbd "C-c c") #'org-capture)
-
-
-(evil-set-initial-state 'org-agenda-mode 'normal)
