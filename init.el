336842e39630d93f6518e19513f34d266a120941
alexmeier2828 <ameier42@gmail.com>
2024-04-21 16:43:34 -0400

    Added minor changes and some more packages

--- init.el
+++ init.el
@@ -8,8 +8,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-agenda-files '("~/org/todo.org" "/Users/alexmeier/org/notes.org"))
  '(package-selected-packages
-   '(magit doom-modeline doom-mode-line powerline which-key marginalia orderless corfu gruvbox-theme evil)))
+   '(anki-editor vterm rustic lsp-mode doom-modeline magit which-key marginalia orderless corfu gruvbox-theme evil)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
