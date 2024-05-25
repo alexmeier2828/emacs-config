2997955eee876f2741f151556c23ad309cf9916b
alexmeier2828 <ameier42@gmail.com>
2024-05-25 16:02:21 -0400

    Commit changes present

--- init.el
+++ init.el
@@ -8,9 +8,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-agenda-files '("~/org/todo.org" "/Users/alexmeier/org/notes.org"))
+ '(custom-safe-themes
+   '("7b8f5bbdc7c316ee62f271acf6bcd0e0b8a272fdffe908f8c920b0ba34871d98" "993aac313027a1d6e70d45b98e121492c1b00a0daa5a8629788ed7d523fe62c1" default))
+ '(org-agenda-files
+   '("/Users/alexmeier/org/notes.org" "/Users/alexmeier/org/calendar-beorg.org" "/Users/alexmeier/org/inbox.org" "/Users/alexmeier/org/todo.org"))
  '(package-selected-packages
-   '(copilot editorconfig quelpa-use-package quelpa straight strait anki-editor vterm rustic lsp-mode doom-modeline magit which-key marginalia orderless corfu gruvbox-theme evil)))
+   '(org-download vscode-dark-plus-theme copilot editorconfig quelpa-use-package quelpa straight strait anki-editor vterm rustic lsp-mode doom-modeline magit which-key marginalia orderless corfu gruvbox-theme evil)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
