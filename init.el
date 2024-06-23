8d92f41787ff4d35aff32bfdfdeccd72a9327c74
alexmeier2828 <ameier42@gmail.com>
2024-06-23 06:58:41 -0400

    pushing recent changes

--- init.el
+++ init.el
@@ -13,7 +13,7 @@
  '(org-agenda-files
    '("/Users/alexmeier/org/notes.org" "/Users/alexmeier/org/calendar-beorg.org" "/Users/alexmeier/org/inbox.org" "/Users/alexmeier/org/todo.org"))
  '(package-selected-packages
-   '(org-download vscode-dark-plus-theme copilot editorconfig quelpa-use-package quelpa straight strait anki-editor vterm rustic lsp-mode doom-modeline magit which-key marginalia orderless corfu gruvbox-theme evil)))
+   '(org-download vscode-dark-plus-theme copilot editorconfig quelpa-use-package quelpa straight strait anki-editor vterm rustic doom-modeline magit which-key marginalia orderless corfu gruvbox-theme evil)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
