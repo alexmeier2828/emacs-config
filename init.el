7346cefc889dc58b8ecf18c16e45521d80c250a3
Alex Meier <ameier42@gmail.com>
2024-09-25 08:46:02 -0400

    Revert "Initial commit for exwm, testing usage of emacs as window manager"
    This reverts commit bd4df6c932bf8e0968dbbe80b4464557c8eb6fd7.

--- init.el
+++ init.el
@@ -10,9 +10,10 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    '("7b8f5bbdc7c316ee62f271acf6bcd0e0b8a272fdffe908f8c920b0ba34871d98" "993aac313027a1d6e70d45b98e121492c1b00a0daa5a8629788ed7d523fe62c1" default))
- '(org-agenda-files '("~/org/notes.org" "/home/alexm/org/inbox.org"))
+ '(org-agenda-files
+   '("/Users/alexmeier/org/notes.org" "/Users/alexmeier/org/calendar-beorg.org" "/Users/alexmeier/org/inbox.org" "/Users/alexmeier/org/todo.org"))
  '(package-selected-packages
-   '(exwm zig-mode org-download vscode-dark-plus-theme copilot editorconfig quelpa-use-package quelpa straight strait anki-editor vterm rustic doom-modeline magit which-key marginalia orderless corfu gruvbox-theme evil)))
+   '(zig-mode org-download vscode-dark-plus-theme copilot editorconfig quelpa-use-package quelpa straight strait anki-editor vterm rustic doom-modeline magit which-key marginalia orderless corfu gruvbox-theme evil)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
