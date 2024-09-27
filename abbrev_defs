56f41934b055d1ce2b22c22ffdccb2014d747153
Alex Meier <ameier42@gmail.com>
2024-09-27 07:16:22 -0400

    accumulated changes in secondary config files

--- abbrev_defs
+++ abbrev_defs
@@ -1,4 +1,9 @@
 ;;-*-coding: utf-8;-*-
+(define-abbrev-table 'global-abbrev-table
+  '(
+    ("bsrc" "#+BEGIN_SRC #END_SRC" nil :count 2)
+   ))
+
 (define-abbrev-table 'org-mode-abbrev-table
   '(
     ("symverse" "sv" nil :count 3)
--- init.el
+++ init.el
@@ -9,10 +9,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("7b8f5bbdc7c316ee62f271acf6bcd0e0b8a272fdffe908f8c920b0ba34871d98" "993aac313027a1d6e70d45b98e121492c1b00a0daa5a8629788ed7d523fe62c1" default))
+   '("d2ab3d4f005a9ad4fb789a8f65606c72f30ce9d281a9e42da55f7f4b9ef5bfc6" "c20728f5c0cb50972b50c929b004a7496d3f2e2ded387bf870f89da25793bb44" "daa27dcbe26a280a9425ee90dc7458d85bd540482b93e9fa94d4f43327128077" "7403e5668f855dc751fe4360cb40f6b3dcd6b535d88db1c9027d2de6e178d6a0" "569f33960d9d639e780ace5dcc2bfa834d4d7fad2084c147a9b1d86138e3014e" "7d1c7ea4f3e73402f012b7011fc4be389597922fa67ad4ec417816971bca6f9d" "c7fd1708e08544d1df2cba59b85bd25263180b19b287489d4f17b9118487e718" "7b8f5bbdc7c316ee62f271acf6bcd0e0b8a272fdffe908f8c920b0ba34871d98" "993aac313027a1d6e70d45b98e121492c1b00a0daa5a8629788ed7d523fe62c1" default))
  '(org-agenda-files '("~/org/notes.org" "/home/alexm/org/inbox.org"))
  '(package-selected-packages
-   '(exwm zig-mode org-download vscode-dark-plus-theme copilot editorconfig quelpa-use-package quelpa straight strait anki-editor vterm rustic doom-modeline magit which-key marginalia orderless corfu gruvbox-theme evil)))
+   '(kanagawa-wave theme-magic kanagawa-themes naga-theme habamax-theme abyss-theme monokai-theme exwm zig-mode org-download vscode-dark-plus-theme copilot editorconfig quelpa-use-package quelpa straight strait anki-editor vterm rustic doom-modeline magit which-key marginalia orderless corfu gruvbox-theme evil)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
